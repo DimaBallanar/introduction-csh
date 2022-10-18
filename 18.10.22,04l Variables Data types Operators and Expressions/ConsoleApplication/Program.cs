0ca35dcfd3f1ef3e7e3cb35881768c53214b5911
Artyom Shevchik <artyom.shevchik16@gmail.com>
2022-10-18 22:53:43 +0300

    tsk2

--- 18.10.22,04l Variables Data types Operators and Expressions/ConsoleApplication/Program.cs
+++ 18.10.22,04l Variables Data types Operators and Expressions/ConsoleApplication/Program.cs
@@ -1,58 +1,23 @@
-﻿// // byte valuebyte = 0xFF;
-// System.Byte valuesystembyte = 127;
-// // sbyte valuesbyte = 127;
-// // System.SByte second = -117;
-// // short valueshort = 23;
-// // System.Int16 valuesystemshort = 33;
-// // int valueint = 320;
-// // System.Int32 valuesystemint = 6;
-// // long valuelong = 5225;
-// // System.Int64 valueSystemLong = 5252;
+﻿//1.Даны переменные a = 10 и b = 3. Найдите остаток от деления a на b.
+// --------------------------------
+// int a = 10;
+// int b = 3;
+// int c = a % b;
+// Console.WriteLine(c);
 
-// // var valueVar = 5262L;
-// // var value = valuesystembyte + valuesystembyte + valuesystembyte; 
-// // string boxer = "127";
-// int valueParse = int.Parse("");
-// int valueConvert = System.Convert.ToInt32(null);
-// System.Console.WriteLine(valueConvert);
-// System.Console.WriteLine(valueParse);
+// 2. Пользователь вводит с клавиатуры 2 числа: основание степени и показатель
+//степени. Необходимо возвести число во введенную степень
 
-// var boxer = Console.ReadLine();
-// int valueConvert = System.Convert.ToInt32(boxer);
-// Console.WriteLine($"Я ввел число: {valueConvert} {valueConvert.GetType()}");
+// P.S. делал 1 способом, но понял что напрямую в степень не возводит порылся в документации 
+// понял, что для того, чтобы возвести в степень, нужно вызвать метод Pow, 
+// прочитал что метод принимает значения типа double, затестил с byte, всё норм
+// int d = Convert.ToByte(Console.ReadLine());
+// int e = Convert.ToByte(Console.ReadLine());
+// int f = d ^ e;
+// Console.WriteLine(f);
+//----------------------------
 
-// Найти остаток от деления value1 от value2 и данный остаток сделать ++
-// Console.Write("Введите число 1: ");
-// var boxer = Console.ReadLine();
-// int value = Convert.ToInt32(boxer);
-// Console.Write("Введите число 2: ");
-// var boxer1 = Console.ReadLine();
-// int value1 = Convert.ToInt32(boxer1);
-// int boxer2 = value % value1;
-// Console.WriteLine(boxer2);
-// int boxer3 = ++boxer2;
-// Console.WriteLine($"Остаток от деления с префексным инкрементом: {boxer3} ");
-// Console.WriteLine($"Остаток от деления с постфиксным инкрементом: {boxer2++} ");
-// Console.WriteLine(boxer2);
+// byte d = Convert.ToByte(Console.ReadLine());
+// byte e = Convert.ToByte(Console.ReadLine());
+// Console.WriteLine($"{d}^{e} = " + Math.Pow(d, e));
 
-// посчитать курс валют и заплатить налог и вывести сколько денег останется после налога
-//--------------------
-// Console.Write("курс валют $: ");
-// var consoleText = Console.ReadLine();
-// decimal rate = Convert.ToDecimal(consoleText);
-// Console.Write("какой Tax: ");
-// consoleText = Console.ReadLine();
-// decimal tax = Convert.ToDecimal(consoleText);
-// Console.Write("количество валюты: ");
-// //consoleText = Console.ReadLine();
-// decimal countmoney = Convert.ToDecimal(Console.ReadLine());
-// // (количество валют * курс валют) * (1 - налог /100)
-// decimal result; 
-// result = (countmoney*rate) * (1 - tax /100);
-// Console.WriteLine(result);
-
-//1.Даны переменные a = 10 и b = 3. Найдите остаток от деления a на b.
-int a = 10;
-int b = 3;
-int c = a % b;
-Console.WriteLine(c);
