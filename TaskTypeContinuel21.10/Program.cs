278b5d775eb3f9c0a8fd54b4c052aadeb8f0e35c
Artyom Shevchik <artyom.shevchik16@gmail.com>
2022-10-21 22:34:54 +0300

    task1

---
+++ TaskTypeContinuel21.10/Program.cs
@@ -0,0 +1,44 @@
+﻿int consoleNumber = Convert.ToInt32(Console.ReadLine());
+string message;
+
+if (consoleNumber < 0)
+{
+    message = "-";
+}
+else if (consoleNumber == 0)
+{
+    message = "0";
+}
+else
+{
+    message = "+";
+}
+
+
+message = consoleNumber < 0 ? "-" : consoleNumber == 0 ? "0" : "+";
+
+System.Console.WriteLine(message);
+
+
+
+--------------------------
+var value = 5;
+
+string? message = null;
+
+if (value == 4)
+{
+    message = "Test";
+}
+
+System.Console.WriteLine(message ?? "Default Text")
+---------------------------
+var value = 5;
+
+string? message = null;
+
+if (value == 4) {
+   message = "Test";
+}
+
+System.Console.WriteLine(message?.ToLower());
