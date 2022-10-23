7936cfa607dbd40821699e4c1e46096db8b25760
Artyom Shevchik <artyom.shevchik16@gmail.com>
2022-10-23 19:36:44 +0300

    task4

--- TaskType2/Program.cs
+++ TaskType2/Program.cs
@@ -2,15 +2,15 @@
 //-------------------------------------------------
 int chis = Convert.ToInt32(Console.ReadLine());
 int chis1 = Convert.ToInt32(Console.ReadLine());
-if (chis>chis1)
+if (chis > chis1)
 {
     Console.WriteLine("1 число максимальное из 2 введённых");
 }
-else if (chis==chis1)
+else if (chis == chis1)
 {
     Console.WriteLine("1 число равно 2");
 }
-else 
+else
 {
     Console.WriteLine("2 число максимальное из 2 введённых");
 }
@@ -37,11 +37,11 @@ switch (chet)
 }
 //----------------------------
 int chet1 = Convert.ToInt32(Console.ReadLine());
-string result = chet switch 
+string result = chet switch
 {
     >= 0 when chet1 % 2 == 0 => "Число чётное",
     > 0 when chet1 % 2 != 0 => "Число нечётное",
-    < 0  when chet1 % 2 == 0 => "Число чётное",
+    < 0 when chet1 % 2 == 0 => "Число чётное",
     < 0 when chet1 % 2 != 0 => "Число нечётное",
 };
 System.Console.WriteLine(result);
@@ -55,31 +55,31 @@ System.Console.WriteLine(result);
 int dennedelya = Convert.ToInt32(Console.ReadLine());
 if (dennedelya == 1)
 {
-Console.WriteLine ($"{dennedelya} --> Понедельник");
+    Console.WriteLine($"{dennedelya} --> Понедельник");
 }
 else if (dennedelya == 2)
 {
-Console.WriteLine ($"{dennedelya} --> Вторник");
+    Console.WriteLine($"{dennedelya} --> Вторник");
 }
 else if (dennedelya == 3)
 {
-Console.WriteLine ($"{dennedelya} --> Среда");
+    Console.WriteLine($"{dennedelya} --> Среда");
 }
 else if (dennedelya == 4)
 {
-Console.WriteLine ($"{dennedelya} --> Четверг");
+    Console.WriteLine($"{dennedelya} --> Четверг");
 }
 else if (dennedelya == 5)
 {
-Console.WriteLine ($"{dennedelya} --> Пятница");
+    Console.WriteLine($"{dennedelya} --> Пятница");
 }
 else if (dennedelya == 6)
 {
-Console.WriteLine ($"{dennedelya} --> Суббота");
+    Console.WriteLine($"{dennedelya} --> Суббота");
 }
 else if (dennedelya == 7)
 {
-Console.WriteLine ($"{dennedelya} --> Воскресенье");
+    Console.WriteLine($"{dennedelya} --> Воскресенье");
 }
 //---------------------------- 
 int dennedelya1 = Convert.ToInt32(Console.ReadLine());
@@ -107,3 +107,28 @@ switch (dennedelya1)
         System.Console.WriteLine($"{dennedelya1} --> Воскресенье");
         break;
 }
+
+//4. Есть строка в 3 символа. Написать проверку если число, вывести true, в
+// противном случае false Char.IsDigit (str2, position))
+var str = Console.ReadLine();
+int str1;
+bool proverka = int.TryParse(str, out str1);
+switch (proverka)
+{
+    case true:
+        System.Console.WriteLine($"{str} Это число");
+        break;
+    case false:
+        System.Console.WriteLine("false Char.IsDigit (str2, position) - Это строка");
+        break;
+}
+//------------------------
+var str2 = Console.ReadLine();
+int str3;
+bool proverka1 = int.TryParse(str2, out str3);
+var result1 = proverka1 switch
+{
+    true => $"{str2} Это число",
+    false => "false Char.IsDigit (str2, position)- Это строка"
+};
+System.Console.WriteLine(result1);
