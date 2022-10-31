1c10d3f47b15c2f8c0f919bfd376f78d0a145276
Artyom Shevchik <artyom.shevchik16@gmail.com>
2022-10-31 13:10:10 +0300

    task2

--- TaskType4/Program.cs
+++ TaskType4/Program.cs
@@ -8,8 +8,23 @@ if (!string.IsNullOrEmpty(stroka))
     Console.WriteLine(stroka.ToLower().Trim() == "hschool");
 }
 else { Console.WriteLine("Вы ввели пустую строку"); }
-
-
+// ______________________________________________________________________
+// 2.Ввести строку. Проверить является ли это число или строка. Если число, то
+// выяснить, четное или нет. Если это строка то преобразовать строку в массив, где
+// каждый символ один элемент массива hschool ’ => [‘h’, ‘s’, ‘c’, ‘h’, ‘o’, ‘o’, ‘l’]
+string? text = Console.ReadLine();
+if (!string.IsNullOrEmpty(text))
+{
+    if (double.TryParse(text, out double number))
+    {
+        Console.WriteLine(number % 2 == 0 ? "чётное" : "не чётное");
+    }
+    else
+    {
+        string[] result = text.Split(".");
+        Console.WriteLine(result.Length);
+    }
+}
 
 
 
