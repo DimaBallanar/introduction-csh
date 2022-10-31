dff2b80aec0fcbbdf42c6463f4890307ced80669
Artyom Shevchik <artyom.shevchik16@gmail.com>
2022-10-31 13:07:32 +0300

    tasktype4task1

---
+++ TaskType4/Program.cs
@@ -0,0 +1,24 @@
+﻿// 1.
+// Ввести строку. Если эта равна “hschool”, вывести true. В противном случае false.
+// Добавить проверки для строк . Удалить лишние пробелы
+
+string? stroka = Console.ReadLine();
+if (!string.IsNullOrEmpty(stroka))
+{
+    Console.WriteLine(stroka.ToLower().Trim() == "hschool");
+}
+else { Console.WriteLine("Вы ввели пустую строку"); }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
