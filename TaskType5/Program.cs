23ef2a0d53f9d970fcb47e322cdf6bec0d3ac1e3
Artyom Shevchik <artyom.shevchik16@gmail.com>
2022-11-04 11:48:16 +0300

    TaskType5task2

--- TaskType5/Program.cs
+++ TaskType5/Program.cs
@@ -13,3 +13,31 @@
         Console.WriteLine(number++);
     }
 }
+
+//2. [1 , 2, 3, 4, 5]. Выведите каждый элемент массива 3 любыми циклами
+int[] array1 = new int[5] { 1, 2, 3, 4, 7 };
+int l = 0;
+while (l <= array1.Length)
+{
+    Console.WriteLine(array1[l]);
+    l++;
+}
+//---------------
+int[] array2 = new int[5] { 1, 2, 3, 4, 7 };
+for (int t = 0; t < array2.Length; t++)
+{
+    Console.WriteLine(array2[t]);
+}
+//---------------
+int[] array3 = new int[5] { 1, 2, 3, 4, 7 };
+foreach (int item in array3)
+{
+    Console.WriteLine(item);
+}
+//---------------
+int[] array45 = new int[5] { 1, 2, 3, 4, 7 };
+
+foreach (int item in array45)
+{
+    Console.WriteLine(item);
+}
