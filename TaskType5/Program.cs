f72489594aec8875c00a5c5540099fcd035fc073
Artyom Shevchik <artyom.shevchik16@gmail.com>
2022-11-07 18:15:47 +0300

    TaskType5 task6

--- TaskType5/Program.cs
+++ TaskType5/Program.cs
@@ -1,20 +1,20 @@
 ﻿// // // 1. Выведите столбец чисел от 1 до 50. for, while
 // // // 
+System.Console.WriteLine("Задача 1");
+Console.WriteLine("Цикл for");
+for (int ind = 1; ind <= 50; ind++)
 {
-    Console.WriteLine("Цикл for");
-    for (int ind = 1; ind <= 50; ind++)
-    {
-        Console.WriteLine(ind);
-    }
-    int number = 1;
-    Console.WriteLine("Цикл while");
-    while (number < 50)
-    {
-        Console.WriteLine(number++);
-    }
+    Console.WriteLine(ind);
+}
+int number = 1;
+Console.WriteLine("Цикл while");
+while (number < 50)
+{
+    Console.WriteLine(number++);
 }
 
 //2. [1 , 2, 3, 4, 5]. Выведите каждый элемент массива 3 любыми циклами
+System.Console.WriteLine("Задача 2");
 int[] array1 = new int[5] { 1, 2, 3, 4, 7 };
 int l = 0;
 while (l <= array1.Length)
@@ -45,7 +45,7 @@ foreach (int item in array45)
 // // 3.
 // // Дан массив с элементами [2, 3, 4, 5]. С помощью 3 циклов найдите произведение
 // // элементов этого массива.
-
+System.Console.WriteLine("Задача 3");
 int[] array4 = new int[4] { 2, 3, 4, 5 };
 int i = 0;
 int result = 1;
@@ -67,12 +67,13 @@ Console.WriteLine(result);
 result = 1;
 foreach (int item in array4)
 {
-result *= item;
+    result *= item;
 }
 Console.WriteLine(result);
 // // -------------------
 // 4.
 // Выведите столбец чисел от 11 до 33 циклом while
+System.Console.WriteLine("Задача 4");
 for (int k = 11; k <= 33; k++)
 {
     System.Console.WriteLine(k);
@@ -83,11 +84,32 @@ while (number4 <= 33)
 {
     System.Console.WriteLine(number4++);
 }
+// // -------------------
 //_______________________________________
 // // 5. Выведите столбец четных чисел в 
 // промежутке от 0 до 100 ( for)
- System.Console.WriteLine("Задача 5");
- for (int l = 0; l <=100; l+=2)
- {
-    Console.WriteLine(l);
- }
+System.Console.WriteLine("Задача 5");
+for (int z = 0; z <= 100; z += 2)
+{
+    Console.WriteLine(z);
+}
+//_______________________________________
+// // 6. С помощью цикла while найдите сумму чисел от 1 до 100.
+System.Console.WriteLine("Задача 6");
+int sum = 1;
+int resultsum = 0;
+while (sum <= 100)
+{
+    resultsum += sum;
+    sum++;
+}
+System.Console.WriteLine(resultsum);
+
+
+
+
+
+
+
+
+
