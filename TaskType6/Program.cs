5b009dc057fc6b2a3fb535b1544f38eaa5ee0e93
Artyom Shevchik <artyom.shevchik16@gmail.com>
2022-11-16 14:12:10 +0100

    TaskType6task2

--- TaskType6/Program.cs
+++ TaskType6/Program.cs
@@ -1,4 +1,7 @@
-﻿System.Console.WriteLine("Задача 1");
+﻿// 1. Пользователь вводит границы интервала из которого программа загадывает число.
+// Написать программу про поиску данного числа. Пользователь пишит число в консоль,
+// программа отвечает
+System.Console.WriteLine("Задача 1");
 System.Console.WriteLine("Введите интервал числа, программа выберет число из этого интервала, вам нужно будет угадать его");
 object? ifnumber1 = Console.ReadLine();
 object? ifnumber2 = Console.ReadLine();
@@ -19,3 +22,37 @@ while (true);
 {
     System.Console.WriteLine("Вы угадали");
 }
+
+// 2.Написать программу, которая подсчитывает количество в строке букв, пробелов, цифр и
+// остальных символов.
+// ввод
+// на массив строки
+System.Console.WriteLine("Задача 2");
+System.Console.WriteLine("Введите строку");
+string? str = Console.ReadLine();
+int symbol = 0;
+int letter = 0;
+int whitespace = 0;
+int isnumber = 0;
+for (int index = 0; index < str.Length; index++)
+{
+    if (Char.IsSymbol(str[index]))
+    {
+        symbol += 1;
+    }
+    else if (char.IsLetter(str[index]))
+    {
+        letter += 1;
+    }
+    else if (char.IsWhiteSpace(str[index]))
+    {
+        whitespace += 1;
+    }
+    else if (char.IsNumber(str[index]))
+    {
+        isnumber += 1;
+    }
+}
+System.Console.WriteLine($"{symbol}-символы,{letter}-буквы, {whitespace}-пробелы, {isnumber}-цифры");
+
+// если остальные символы имеется ввиду не специальные, то тогда 2 вариант
